3bfc3340dc40696d59a398894d60d810d9dbefae
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2019-06-11 15:24:23 -0700

    test(compiler-cli): add tests for component analysis API transform

---
+++ packages/compiler-cli/src/ngtsc/indexer/test/transform_spec.ts
@@ -0,0 +1,84 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {BoundTarget, DirectiveMeta} from '@angular/compiler';
+import {ComponentAnalysisContext} from '../src/context';
+import {getTemplateIdentifiers} from '../src/template';
+import {generateAnalysis} from '../src/transform';
+import * as util from './util';
+
+/**
+ * Adds information about a component to a context.
+ */
+function populateContext(
+    context: ComponentAnalysisContext, component: string, selector: string, template: string,
+    scope: BoundTarget<DirectiveMeta>| null) {
+  const declaration = util.getComponentDeclaration(component);
+  const parsedTemplate = util.getParsedTemplate(template);
+  context.addComponent(declaration, selector, parsedTemplate, scope);
+}
+
+describe('generateAnalysis', () => {
+  it('should emit analysis information', () => {
+    const context = new ComponentAnalysisContext();
+    populateContext(context, 'class C {};', 'c-selector', '<div>{{foo}}</div>', null);
+    const analysis = generateAnalysis(context);
+
+    expect(analysis).toEqual([{
+      name: 'C',
+      selector: 'c-selector',
+      declaration: util.getComponentDeclaration('class C {};'),
+      sourceFile: util.TESTFILE,
+      content: 'class C {};',
+      template: {
+        identifiers: getTemplateIdentifiers(util.getParsedTemplate('<div>{{foo}}</div>')),
+        usedComponents: [],
+      },
+    }]);
+  });
+
+  it('should emit used components', () => {
+    const context = new ComponentAnalysisContext();
+
+    const templateA = '<b-selector></b-selector>';
+    const scopeA = util.bindTemplate(templateA, [{selector: 'b-selector', name: 'B'}]);
+    populateContext(context, 'class A {};', 'a-selector', '<div>{{foo}}</div>', scopeA);
+
+    const templateB = '<a-selector></a-selector>';
+    const scopeB = util.bindTemplate(templateB, [{selector: 'a-selector', name: 'A'}]);
+    populateContext(context, 'class B {};', 'b-selector', templateB, scopeB);
+
+    const analysisA = {
+      name: 'A',
+      selector: 'a-selector',
+      declaration: util.getComponentDeclaration('class A {};'),
+      sourceFile: util.TESTFILE,
+      content: 'class A {};',
+      template: {
+        identifiers: getTemplateIdentifiers(util.getParsedTemplate('<div>{{foo}}</div>')),
+        usedComponents: new Array(),
+      }
+    };
+    const analysisB = {
+      name: 'B',
+      selector: 'b-selector',
+      declaration: util.getComponentDeclaration('class B {};'),
+      sourceFile: util.TESTFILE,
+      content: 'class B {};',
+      template: {
+        identifiers: getTemplateIdentifiers(util.getParsedTemplate(templateB)),
+        usedComponents: [analysisA],
+      }
+    };
+    analysisA.template.usedComponents.push(analysisB);
+
+    const analysis = generateAnalysis(context);
+
+    expect(analysis).toEqual([analysisA, analysisB]);
+  });
+});
--- packages/compiler-cli/src/ngtsc/indexer/test/util.ts
+++ packages/compiler-cli/src/ngtsc/indexer/test/util.ts
@@ -6,18 +6,22 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {TmplAstNode, parseTemplate} from '@angular/compiler/src/compiler';
+import {BoundTarget, CssSelector, DirectiveMeta, R3TargetBinder, SelectorMatcher, TmplAstNode, parseTemplate} from '@angular/compiler/src/compiler';
 import * as ts from 'typescript';
 
 import {ClassDeclaration} from '../../reflection';
 
+
+
+/** Dummy file URL */
+export const TESTFILE = 'TESTFILE';
 
 /**
  * Creates a class declaration from a component source code.
  */
 export function getComponentDeclaration(component: string): ClassDeclaration {
   const sourceFile = ts.createSourceFile(
-      'TESTFILE', component, ts.ScriptTarget.ES2015,
+      TESTFILE, component, ts.ScriptTarget.ES2015,
       /* setParentNodes */ true);
 
   return sourceFile.statements.filter(ts.isClassDeclaration)[0] as ClassDeclaration;
@@ -27,5 +31,27 @@ export function getComponentDeclaration(component: string): ClassDeclaration {
  * Parses a template source code.
  */
 export function getParsedTemplate(template: string): TmplAstNode[] {
-  return parseTemplate(template, 'TESTFILE').nodes;
+  return parseTemplate(template, TESTFILE).nodes;
+}
+
+/**
+ * Binds information about a component on a template (a target). The BoundTarget
+ * describes the scope of the template and can be queried for directives the
+ * template uses.
+ */
+export function bindTemplate(template: string, components: Array<{selector: string, name: string}>):
+    BoundTarget<DirectiveMeta> {
+  const matcher = new SelectorMatcher<DirectiveMeta>();
+  components.forEach(({selector, name}) => {
+    matcher.addSelectables(CssSelector.parse(selector), {
+      name,
+      isComponent: true,
+      inputs: {},
+      outputs: {},
+      exportAs: null,
+    });
+  });
+  const binder = new R3TargetBinder(matcher);
+
+  return binder.bind({template: getParsedTemplate(template)});
 }
